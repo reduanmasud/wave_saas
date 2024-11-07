d68f74e35c317522bb94dc083d47d1f8a4eb6e2e
Reduan Masud <reduanmasudcse@gmail.com>
2024-11-07 23:13:03 +0600

    first commit

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+use Illuminate\Support\Facades\Route;
+use Wave\Facades\Wave;
+
+// Wave routes
+Wave::routes();
+
+Route::get('role', function(){
+    // dd(\App\Models\User::find(2)->roles);
+});
